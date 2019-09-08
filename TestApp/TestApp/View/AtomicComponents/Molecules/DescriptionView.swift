fe29b47727e3286f6ff5566900b39b14cd239fd4
armenm <armen.mkrtchian@gmail.com>
2019-09-08 17:36:56 -0400

    Removed test files

--- TestApp/TestApp/View/AtomicComponents/Molecules/DescriptionView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DescriptionView.swift
-//  TestApp
-//
-//  Created by Armen Mkrtchyan on 2019-09-08.
-//  Copyright © 2019 Mercari. All rights reserved.
-//
-
-import Foundation
--- TestApp/TestApp/View/Sections/DescriptionSection.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DescriptionSection.swift
-//  TestApp
-//
-//  Created by Armen Mkrtchyan on 2019-09-08.
-//  Copyright © 2019 Mercari. All rights reserved.
-//
-
-import Foundation
