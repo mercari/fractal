60a17c7dd2ae68b2125879096583885fb80a3b4a
Jon Bott <herbal7ea@mercari.com>
2019-09-08 22:15:31 +0900

    Updated Phase I with notes

--- TestApp/TestApp/Phases/Phase1.swift
+++ TestApp/TestApp/Phases/Phase1.swift
@@ -10,24 +10,41 @@ import Foundation
 
 // Add to YogaViewController
 
+
 //private func setSections() {
 //    dataSource.sections = [
+//        //1 - Similar to SwiftUI Spacer
 //        spacing(30.0),
+//
+//        //2 - Custom Carousel with multiple buttons
 //        buttonCarousel(with: self.presenter.yogaTypes, selectionClosure: { (index) in
+//            //Optional Tasks
+//            //Update UI: set text, run animations, etc.
+//            //Notify Presenter/VM of button tapped.
+//            //Log Event either from UI layer or Presenter/VM layer
 //            print("Button Selected at Index:", index)
 //        }),
+//
+//        //Spacing defaults to "medium" in your branding system - Yoga brand line ~38
 //        spacing(),
+//
+//        //3 - Simple Label Section
 //        headline("Popular Lessons"),
-//        yogaEventsCarousel(with: self.presenter.popularEvents,
-//                           selectionClosure: presenter.eventSelected),
+//
+//        //4 - Custom Carousel - self encapsulated data
+//        //Showing another example, where we pass the event to the Presenter.
+//        yogaEventsCarousel(with: self.presenter.popularEvents, selectionClosure: presenter.eventSelected),
 //        headline("New Lessons"),
-//        yogaEventsCarousel(with: self.presenter.newEvents,
-//                           selectionClosure: presenter.eventSelected),
+//        yogaEventsCarousel(with: self.presenter.newEvents, selectionClosure: presenter.eventSelected),
 //        spacing(60.0),
+//
+//        //5 - Simple Button with event
 //        singleButton("Try Premium", tappedClosure: {
 //            print("Premium Selected")
 //        })
 //    ]
 //}
+
+
 
 
